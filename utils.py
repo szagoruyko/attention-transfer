f4fd34ad673fa11779dcb96b515cbce7b7de3aa9
Sergey Zagoruyko <zagoruyko2@gmail.com>
2018-05-20 21:28:08 +0200

    cifar compatible with pytorch0.4

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 from nested_dict import nested_dict
-from collections import OrderedDict
 from functools import partial
 import torch
-import torch.cuda.comm as comm
-from torch.autograd import Variable
-from torch.nn.init import kaiming_normal
+from torch.nn.init import kaiming_normal_
 from torch.nn.parallel._functions import Broadcast
 from torch.nn.parallel import scatter, parallel_apply, gather
 import torch.nn.functional as F
@@ -25,63 +22,64 @@ def at_loss(x, y):
 
 def cast(params, dtype='float'):
     if isinstance(params, dict):
-        return {k: cast(v, dtype) for k,v in list(params.items())}
+        return {k: cast(v, dtype) for k,v in params.items()}
     else:
-        return getattr(params.cuda(), dtype)()
+        return getattr(params.cuda() if torch.cuda.is_available() else params, dtype)()
 
 
 def conv_params(ni, no, k=1):
-    return cast(kaiming_normal(torch.Tensor(no, ni, k, k)))
+    return kaiming_normal_(torch.Tensor(no, ni, k, k))
 
 
 def linear_params(ni, no):
-    return cast({'weight': kaiming_normal(torch.Tensor(no, ni)), 'bias': torch.zeros(no)})
+    return {'weight': kaiming_normal_(torch.Tensor(no, ni)), 'bias': torch.zeros(no)}
 
 
 def bnparams(n):
-    return cast({'weight': torch.rand(n), 'bias': torch.zeros(n)})
+    return {'weight': torch.rand(n),
+            'bias': torch.zeros(n),
+            'running_mean': torch.zeros(n),
+            'running_var': torch.ones(n)}
 
 
-def bnstats(n):
-    return cast({'running_mean': torch.zeros(n), 'running_var': torch.ones(n)})
-
-
-def data_parallel(f, input, params, stats, mode, device_ids, output_device=None):
+def data_parallel(f, input, params, mode, device_ids, output_device=None):
+    device_ids = list(device_ids)
     if output_device is None:
         output_device = device_ids[0]
 
     if len(device_ids) == 1:
-        return f(input, params, stats, mode)
+        return f(input, params, mode)
 
-    def replicate(param_dict, g):
-        replicas = [{} for d in device_ids]
-        for k,v in param_dict.items():
-            for i,u in enumerate(g(v)):
-                replicas[i][k] = u
-        return replicas
+    params_all = Broadcast.apply(device_ids, *params.values())
+    params_replicas = [{k: params_all[i + j*len(params)] for i, k in enumerate(params.keys())}
+                       for j in range(len(device_ids))]
 
-    params_replicas = replicate(params, lambda x: Broadcast(device_ids)(x))
-    stats_replicas = replicate(stats, lambda x: comm.broadcast(x, device_ids))
-
-    replicas = [partial(f, params=p, stats=s, mode=mode)
-                for p,s in zip(params_replicas, stats_replicas)]
+    replicas = [partial(f, params=p, mode=mode)
+                for p in params_replicas]
     inputs = scatter([input], device_ids)
     outputs = parallel_apply(replicas, inputs)
     return gather(outputs, output_device)
 
 
-def flatten_params(params):
-    return OrderedDict(('.'.join(k), Variable(v, requires_grad=True))
-                       for k, v in nested_dict(params).iteritems_flat() if v is not None)
+def flatten(params):
+    return {'.'.join(k): v for k, v in nested_dict(params).items_flat() if v is not None}
 
 
-def flatten_stats(stats):
-    return OrderedDict(('.'.join(k), v)
-                       for k, v in nested_dict(stats).iteritems_flat())
-
-def batch_norm(x, params, stats, base, mode):
+def batch_norm(x, params, base, mode):
     return F.batch_norm(x, weight=params[base + '.weight'],
                         bias=params[base + '.bias'],
-                        running_mean=stats[base + '.running_mean'],
-                        running_var=stats[base + '.running_var'],
+                        running_mean=params[base + '.running_mean'],
+                        running_var=params[base + '.running_var'],
                         training=mode)
+
+
+def print_tensor_dict(params):
+    kmax = max(len(key) for key in params.keys())
+    for i, (key, v) in enumerate(params.items()):
+        print(str(i).ljust(5), key.ljust(kmax + 3), str(tuple(v.shape)).ljust(23), torch.typename(v), v.requires_grad)
+
+
+def set_requires_grad_except_bn_(params):
+    for k, v in params.items():
+        if not k.endswith('running_mean') and not k.endswith('running_var'):
+            v.requires_grad = True
